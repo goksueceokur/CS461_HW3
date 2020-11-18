bab714c04b39bf7ceaf4b2a480c361af2cff8bc1
Yüce <yuceyuce99@gmail.com>
2020-11-18 14:22:10 +0300

    Implement trace mod to alpha beta

--- alpha_beta.py
+++ alpha_beta.py
@@ -1,59 +1,121 @@
 from math import inf
 
-TRACE_MOD = True
-
 TREE_DEPTH = 2
 BRANCH_FACTOR = 3
-EVAL_LIST = [5, 3, 1, 2, 5, 4, 1, 3, 3]
-ALPHABET = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
+ALPHABET = ['A','B','C','D','E','F','G','H','I']
 
-def alpha_beta(is_minimizing, depth = 0, index = 0, alpha=-inf, beta=inf):
+def alpha_beta(input_list, is_minimizing, depth = 0, index = 0, alpha=-inf, beta=inf, trace_mod=False):
     if depth == TREE_DEPTH:
-        return EVAL_LIST[index - 4]
-        
+        parent_index = (index - 1) // BRANCH_FACTOR
+        child_no = index - parent_index * BRANCH_FACTOR
+        child_direction = ""
+        if child_no == 1:
+            child_direction = "LEFT"
+        elif child_no == 2:
+            child_direction = "MIDDLE"
+        elif child_no == 3:
+            child_direction = "RIGHT"
+
+        non_leaf_nodes = sum(BRANCH_FACTOR ** i for i in range(TREE_DEPTH ))
+        value = int(input_list[index - non_leaf_nodes])
+
+        if trace_mod:
+            print(('\t' * depth), child_direction, "node at depth", depth, "has a value", value)
+
+        return child_direction, value
+
+    if trace_mod:
+        turn = "minimizer" if is_minimizing else "maximizer"
+        print(('\t' * depth), "Turn of", turn)    
+    
     if is_minimizing:
         count = 0 #!
         min_children = inf
+        direction = ""
         while count < BRANCH_FACTOR and alpha < beta:
-            temp_value = alpha_beta(not is_minimizing, depth + 1, index * BRANCH_FACTOR + count + 1, alpha, beta)
-            min_children = min(min_children, temp_value)
+            if trace_mod:
+                if count == 0:
+                    print(('\t' * depth), "Going LEFT")
+                elif count == 1:
+                    print(('\t' * depth), "Going MIDDLE")
+                elif count == 2:
+                    print(('\t' * depth), "Going RIGHT")
+
+            child_direction, temp_value = alpha_beta(input_list, not is_minimizing, depth + 1, index * BRANCH_FACTOR + count + 1, alpha, beta, trace_mod)
+
+            if temp_value < min_children:
+                min_children = temp_value
+                if count == 0:
+                    direction = "LEFT"
+                elif count == 1:
+                    direction = "MIDDLE"
+                elif count == 2:
+                    direction = "RIGHT"
+
             beta = min(beta, temp_value)
             count += 1
 
         if alpha >= beta:
             while count < BRANCH_FACTOR:
-                print_pruned(depth + 1, index * BRANCH_FACTOR + count + 1)
+                print_pruned(input_list, depth + 1, index * BRANCH_FACTOR + count + 1)
                 count += 1
 
-        return min_children
+        if trace_mod:
+            print(('\t' * depth), turn, "chose", direction, "node at depth", depth, "and has a value", min_children)
+
+        return direction, min_children
 
     if not is_minimizing:
         count = 0 #!
         max_children = -inf
+        direction = ""
         while count < BRANCH_FACTOR and alpha < beta:
-            temp_value = alpha_beta(not is_minimizing, depth + 1, index * BRANCH_FACTOR + count + 1, alpha, beta)
-            max_children = max(max_children, temp_value)
+            child_direction, temp_value = alpha_beta(input_list, not is_minimizing, depth + 1, index * BRANCH_FACTOR + count + 1, alpha, beta, trace_mod)
+            
+            if temp_value > max_children:
+                max_children = temp_value
+                if count == 0:
+                    direction = "LEFT"
+                elif count == 1:
+                    direction = "MIDDLE"
+                elif count == 2:
+                    direction = "RIGHT"
+
             alpha = max(alpha, temp_value)
             count += 1
 
         if alpha >= beta:
             while count < BRANCH_FACTOR:
-                print_pruned(depth + 1, index * BRANCH_FACTOR + count + 1)
+                print_pruned(input_list, depth + 1, index * BRANCH_FACTOR + count + 1)
                 count += 1
+                
+        if trace_mod:
+            print(('\t' * depth), turn, "chose", direction, "node at depth", depth, "and has a value", max_children)
 
-        return max_children
+        return direction, max_children
 
-def print_pruned(depth, index):
+def print_pruned(input_list, depth, index):
     if depth == TREE_DEPTH:
-        print("Node {} with value {} is pruned".format(ALPHABET[index - 4], EVAL_LIST[index - 4]))
+        non_leaf_nodes = sum(BRANCH_FACTOR ** i for i in range(TREE_DEPTH ))
+        print(('\t' * depth), "Node {} with value {} is pruned".format(ALPHABET[index - non_leaf_nodes], input_list[index - non_leaf_nodes]))
         return
 
+    else:
+        print("Error")
 
 def main():
     takenInput = input("give input: ")
-    EVAL_LIST = takenInput.split(" ")
-    print(alpha_beta(False))
-    pass
+    userInput = takenInput.split(" ")
+    for i in userInput:
+        i= int(i)
+    starter = "maximizer" # other option is "minimizer"
+    choice = input("Do you want to enable single stepping? Y/N \n") in ["Y", "y", "yes", "Yes"]
+    
+    result = alpha_beta(userInput, False, trace_mod = choice)
+    if choice:  
+        print("--------------")
+
+    print("Selected move for", starter, "is", result[0], "and has a value", result[1])
 
 if __name__ == '__main__':
     main()
--- minimax.py
+++ minimax.py
@@ -89,49 +89,6 @@ def minimax(input_list, starter, depth = 0, index = 0, trace_mod=False):
         return None, None
 
 
-# def Minimax(depth, breadt, starter, userInput):
-#     if starter== "minimizer":
-#         opponent = "maximizer"
-    
-#     else:
-#         opponent = "minimizer"
-
-#     List = userInput
-#     j= depth
-
-#     while j != 0:
-#         if j%2 ==0:
-#             player= opponent
-    
-#         else:
-#             player = starter
-
-#         while len(List) != breadt** (j-1):
-#             group =[]
-#             n= breadt
-
-#             while n != 0:
-#                 group.append(List[0])
-#                 List.remove(List[0])
-#                 n= n-1
-
-#             if player == "maximizer":
-#                 maxi= group[0]
-
-#                 for k in group:
-#                     maxi = max( maxi, k)
-#                 List.append(maxi)
-                
-#             else:
-#                 mini = group[0]
-#                 for k in group:
-#                     mini = min( mini, k)
-#                 List.append(mini)
-#         j -=1
-
-#     print(List)
-
-
 def main():
     takenInput = input("give input: ")
     userInput = takenInput.split(" ")
